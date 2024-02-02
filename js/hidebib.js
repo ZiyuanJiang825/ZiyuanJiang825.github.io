4c92760a2849aaf4815c50abe1f5b6a78161e74c
Ziyuan(Jack) Jiang <48230839+ZiyuanJiang825@users.noreply.github.com>
2024-02-02 10:33:31 -0600

    update

--- js/hidebib.js
+++ js/hidebib.js
@@ -1,23 +1,21 @@
-function hideallbibs()
-{
-    var el = document.getElementsByTagName("div") ;
-    for (var i = 0 ; i < el.length ; ++i) {
-        var bib = el[i].getElementsByTagName("pre") ;
-        if (bib.length > 0) {
-            bib [0] .style.display = 'none' ;
-        }
+function hideallbibs() {
+  var el = document.getElementsByTagName("div");
+  for (var i = 0; i < el.length; ++i) {
+    var bib = el[i].getElementsByTagName("pre");
+    if (bib.length > 0) {
+      bib[0].style.display = "none";
     }
+  }
 }
 
-function togglebib(paperid)
-{
-    var paper = document.getElementById(paperid) ;
-    var bib = paper.getElementsByTagName('pre') ;
-    if (bib.length > 0) {
-        if (bib [0] .style.display == 'none') {
-            bib [0] .style.display = 'block' ;
-        } else {
-            bib [0] .style.display = 'none' ;
-        }
+function togglebib(paperid) {
+  var paper = document.getElementById(paperid);
+  var bib = paper.getElementsByTagName("pre");
+  if (bib.length > 0) {
+    if (bib[0].style.display == "none") {
+      bib[0].style.display = "block";
+    } else {
+      bib[0].style.display = "none";
     }
+  }
 }
